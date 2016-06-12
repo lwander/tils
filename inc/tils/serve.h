db7ddb45e22ab849f6694dab5d0d8a756fd5d624
Lars Wander <lars.wander@gmail.com>
2016-06-12 13:32:37 -0400

    Update c-http -> tils holdouts

--- inc/tils/serve.h
+++ inc/tils/serve.h
@@ -1,18 +1,18 @@
 /*
- *  This file is part of c-http.
+ *  This file is part of tils.
  *
- *  c-http is free software: you can redistribute it and/or modify
+ *  tils is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  c-http is distributed in the hope that it will be useful,
+ *  tils is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with c-http.  If not, see <http://www.gnu.org/licenses/>
+ *  along with tils.  If not, see <http://www.gnu.org/licenses/>
  */
 
 /**
@@ -26,7 +26,7 @@
 #ifndef _SERVE_H_
 #define _SERVE_H_
 
-#define SERVER_STRING "Server: lwander-c-http/0.0.1\r\n"
+#define SERVER_STRING "Server: lwander-tils/0.0.1\r\n"
 
 #include <stdio.h>
 #include <stdlib.h>
--- src/tils/conn.c
+++ src/tils/conn.c
@@ -1,18 +1,18 @@
 /*
- *  This file is part of c-http.
+ *  This file is part of tils.
  *
- *  c-http is free software: you can redistribute it and/or modify
+ *  tils is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  c-http is distributed in the hope that it will be useful,
+ *  tils is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with c-http.  If not, see <http://www.gnu.org/licenses/>
+ *  along with tils.  If not, see <http://www.gnu.org/licenses/>
  */
 
 /**
