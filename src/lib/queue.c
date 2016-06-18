489503a51c1b25e01bc258e7b1991738925f4ca7
Lars Wander <lars.wander@gmail.com>
2016-06-18 15:19:57 -0400

    Queue operation description

--- src/lib/queue.c
+++ src/lib/queue.c
@@ -16,9 +16,24 @@
  */
 
 /**
- * @file src/lib/queue_private.h
+ * @file src/lib/queue.c
  *
  * @brief Queue data structure implementation
+ *
+ * The queue works by maintaining 3 internal queues. At any time, 2 are in
+ * use. One is being added to by the producer, and the other is being removed
+ * from by the consumer. These queues are called Q_P and Q_C respectively, and
+ * stand for the number labeling each buffer.
+ *
+ * If the Q_C is empty, we increment Q_C
+ *
+ * Q_C := Q_C + 1 % 3. 
+ *
+ * Now if Q_C != Q_P, then nothing special happens. If Q_C == Q_P, we need to
+ * increment Q_P atomically. The trick is that insertions and removals from 
+ * each queue are atomic as well, as in this scenario it is possible that an
+ * insertion and a removal are happening concurrently, so it is critical that
+ * the same piece of data is not inserted and removed at the same time.
  *
  * @author Lars Wander 
  */
