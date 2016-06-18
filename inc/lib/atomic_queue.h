fd387ddbcfaeb1afabb8d14b4405ec5eb70909d9
Lars Wander <lars.wander@gmail.com>
2016-06-18 15:10:25 -0400

    Adding lock free queue

---
+++ inc/lib/atomic_queue.h
@@ -0,0 +1,37 @@
+/*
+ *  This file is part of tils.
+ *
+ *  tils is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  tils is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with tils.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+/**
+ * @file lib/atomic_queue.h
+ *
+ * @brief Atomic, lock-free, single producer single consumer queue
+ *
+ * @author Lars Wander
+ */
+
+#ifndef _QUEUE_H_
+#define _QUEUE_H_
+
+struct _queue;
+typedef struct queue queue_t;
+
+queue_t *queue_new();
+int queue_insert(queue_t *q, void *value);
+int queue_remove(queue_t *q, void **value);
+void queue_free(queue_t *q, void (*free_value)(void *));
+
+#endif /* _QUEUE_H_ */
